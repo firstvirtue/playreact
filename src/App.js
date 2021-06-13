94e55729e85fbbe4ac8af98d53518ac011ca4d90
Jake Lee <firstvirtue@gmail.com>
2021-06-14 00:40:31 +0900

    Add index.html script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    window.sendMessage = (type, param) => {
+      console.log('유휴!', type, param);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
